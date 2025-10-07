2a885a46904d5f8e8ce5fb7d2f06a41cda641fc4
susheel kumar <130265075+Susheeelk@users.noreply.github.com>
2025-10-07 09:44:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port = process.env.PORT || 5000
 app.use(cors({
-    origin: "social-media-alpha-ruby.vercel.app",
+    origin: "https://social-media-alpha-ruby.vercel.app",
     credentials: true
 }))
 app.use(express.json())
